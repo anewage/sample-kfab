371fd07c7a0b022dc4210a73101b225ad4c9bc04
anewage <amirh2008@gmail.com>
2016-03-24 03:06:33 +0430

    initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,206 @@
+@extends('welcome')
+
+@section('content')
+
+
+    <div id="innehall_hoger">
+        <div class="innehall_hoger_kol1">
+            <div class="ruta_runda_horn" style="width:330px;">
+                <div class="ruta_runda_horn_innehall">
+                    <strong>ک-فاب اسکاندیناویا</strong> یک سازنده سوئدی است که هنر کلاسیک را با معماری مدرن درهم آمیخته.</div>
+            </div>
+            <p>
+                &nbsp;</p>
+
+
+            <div id="sliderFrame" style="margin-top:-24px">
+                <div id="slider">
+                    <!--a href="#" target="_blank"-->
+                    <img src="/images/image-slider/ny_slider_1.png" /> <!-- alt="G&aring;r att lägg in lite text också..." />
+					            <!--/a-->
+                </div>
+                <div style="clear:both;height:0;"></div>
+            </div>
+
+            <div id="nyheter" style="margin-top:-1px">
+
+                <h4>پیشِ رو</h4>
+
+
+                <span class="small">تا آخر تعطیلات نوروز</span>
+                <br/>
+                <strong>نهایی کردن تغییرات و طراحی های وبسایت</strong>
+
+
+
+                <div style="margin-top:0px">
+
+                    <a href="/UPCOMING">
+                        بایستی طراحی به پایان برسد.<br/>
+                        بیشتر بخوانید
+                    </a>
+
+                    <br/><br/>
+                </div>
+
+                <strong><a href="/UPCOMING ">بیشتر</a></strong>
+            </div>
+        </div>
+
+        <div class="innehall_hoger_kol2" style="margin-top:-5px;">
+            <h4 style="margin-top: 0px; margin-bottom: 10px">محصولات جدید</h4>
+
+
+
+            <div id="products">
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="http://k-fab.eu/ProductHighslide.asp?sku=66536" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="/userfiles/ProductImages/Thumbs/66536_2016-01-03_15-39-30.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Carling</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66457" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66457_2016-01-03_15-21-13.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Denise</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=72029" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/72029_2016-01-03_17-17-33.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Dixie</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66480" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66480_2016-01-03_15-23-56.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Durham</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=16892" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/16892_2016-01-03_18-16-34.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Emily</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66499" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66499_2016-01-03_14-40-31.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Harold</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=79988" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/79988_2016-01-03_14-43-35.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Havanna</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66560" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66560_2016-01-03_15-52-16.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Kevin</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=69454" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/69454_2016-01-03_17-25-01.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Li</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66528" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66528_2016-01-03_18-22-07.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Roden</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66487" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66487_2016-01-03_14-55-42.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Royce</p>
+                    </a>
+                </div>
+
+                <div class="produktruta" style="text-align: center;">
+                    <a href="/ProductHighslide.asp?sku=66489" onclick="return hs.htmlExpand(this, { objectType: 'iframe', width:800 } )">
+
+                        <img src="http://k-fab.eu/userfiles/ProductImages/Thumbs/66489_2016-01-03_14-57-13.jpg" border="0" />
+
+                        <div class="clearfix"></div>
+                        <p class="ThumbName">Sheldon</p>
+                    </a>
+                </div>
+
+            </div>
+            <div style="clear:both"></div>
+
+
+            <p style="margin:10px 0 0 0"><a href="/OUR_PRODUCTS"><strong>اخبار بیشتر</strong></a></p>
+
+
+            <!--div id="nyhetsbrevsruta">
+                <a style="float: left; margin-left: 5px;" href="nyhetsbrev_regga.asp" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )">
+                Subscribe to our newsletter!</a>
+            </div-->
+
+            <form id="frmsok" method="post" action="/OUR_PRODUCTS/sok/" style="margin-top:20px">
+                <input id="sokruta" name="sokruta" value="Search product" onclick="if(sokclear==0) {document.getElementById('sokruta').value='';sokclear=1;}" style="width:200px;" />
+                <input name="Skicka" type="submit" class="skickaknapp" value="Search" />
+            </form>
+        </div>
+    </div>
+
+
+
+
+    <div class="clear"></div>
+    <!-- SIDA FÖR 2 KOLUMNER SLUTAR -->
+
+    <!-- SIDA FÖR 1 KOLUMN STARTAR -->
+    <!--h1>Rubrik</h1><p>text</p-->
+    <!-- SIDA FÖR 1 KOLUMN SLUTAR -->
+
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ک-فاب اسکاندیناویا در ایران</title>
+
+        <meta http-equiv="Content-Language" content="FA" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css\style.css" />
+        <script src="https://secure.adnxs.com/seg?add=2171424&other=73&t=1" type="text/javascript"></script>
+
+    </head>
+
+    <body>
+    <div id="topp">
+        <div id="topp_lampor">
+            <div id="topp_vanster">
+                <a href="/">
+                    <img height="160" src="/images/logga_topp.png" width="114" /></a></div>
+            <div id="topp_mitten">
+                <p class="h1">یک انتخاب روشن‌تر</p>
+            </div>
+            <div id="topp_hoger"></div>
+        </div>
+        <div class="clear"></div>
+    </div>
+
+
+    <div id="innehall">
+
+        <!-- SIDA FÖR 2 KOLUMNER STARTAR -->
+        <div id="innehall_vanster" style="z-index:2;position:relative">
+
+            <p class="meny"><a class="meny1" href="/OUR_OWN_DESIGN">طراحی خود ما</a></p>
+
+            <p class="meny"><a class="meny1" href="/START_PAGE">صفحه اصلی</a></p>
+
+            <p class="meny"><a class="meny1" href="/OUR_PRODUCTS">محصولات ما</a></p>
+
+            <p class="meny"><a class="meny1" href="/INSPIRATION">الهام‌گیری</a></p>
+
+            <p class="meny"><a class="meny1" href="/RETAILERS">فروشندگان</a></p>
+
+            <p class="meny"><a class="meny1" href="/ABOUT_K-FAB">درباره ک-فاب</a></p>
+
+            <p class="meny"><a class="meny1" href="/FOR_A_BRIGHTER_LIFE">برای یک زندگی روشن‌تر</a></p>
+
+            <p class="meny"><a class="meny1" href="/UPCOMING">پیش‌رو</a></p>
+
+            <p class="meny"><a class="meny1" href="/CONTACT">ارتباط با ما</a></p>
+
+
+            <br/>
+            <p style="text-align:center; margin:0; padding:0;">
+                <a href="http://k-fab.eu/?lan=1"><img src="/images/SWE.gif" style="height:16px;margin-left:4px;margin-right:5px;"/></a>
+                <a href="http://k-fab.eu/?lan=2"><img src="/images/ENG.gif" style="height:16px;margin-right:5px;"/></a>
+                <a href="http://k-fab.ir"><img src="/images/IRN.png" style="height:16px;margin-right:5px;"/></a>
+            </p>
+            <br/><br/>
+
+            <p style="text-align:center; margin:0; padding:0;">
+                <img style="border:0px;width:74px" id="img_98_58_px" src="http://merit.soliditet.se/merit/imageGenerator/display?lang=SE&country=SE&cId=iF%2B3LPZGGueU2%2BzPZ7KISw%3D%3D&cUid=odmN9nHdst0%3D&imgType=img_98_58_px" />
+            </p>
+            <!--img style="border:0px;" id="img_98_58_px" src="http://merit.soliditet.se/merit/imageGenerator/display?lang=SE&country=SE&cId=iF%2B3LPZGGueU2%2BzPZ7KISw%3D%3D&cUid=odmN9nHdst0%3D&imgType=img_144_135_px" /-->
+
+        </div>
+
+
+
+        <div id="STARTSIDAN">
+            @yield('content')
+        </div>
+        {{--<img src="/images/lampor_nere_vanster.png" style="margin-top:-20px;margin-left:0px;z-index:1;position:relative"/>--}}
+        <div class="clear"></div>
+    </div>
+    </body>
+</html>
